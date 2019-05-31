186698908d45e8efead45cbf221ca30cdaf2e764
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-05-31 22:18:26 +0100

    Added lint and test to circleci

---
+++ lambdas/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euf -o pipefail
+
+failure=0
+test_script="${1:-test}"
+
+for FUNCTION_NAME in $(find . -type f -name Pipfile | awk -F '/' '{print $2;}' | sort)
+do
+  echo "Testing $FUNCTION_NAME"
+  export FUNCTION_NAME
+  docker-compose -f docker-compose.util.yml run --rm pipenv install
+  docker-compose -f docker-compose.util.yml run --rm pipenv run $test_script || failure=1
+done
+
+exit $failure
