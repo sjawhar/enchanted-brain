f960b0e9014c0a02cff53f235ccfec6e600ea40e
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-03 00:40:22 +0200

    Add route mapping for onConnect and onChoiceMade

--- concert/app/src/functions/on_disconnect/index.py
+++ concert/app/src/functions/on_disconnect/index.py
@@ -8,6 +8,7 @@ client_sqs = boto3.client("sqs")
 def handler(event, context):
     connection_id = event["requestContext"]["connectionId"]
 
+    # TODO: Read queue URL, event source mapping UUID, and subscription ARN
     client_sqs.delete_queue(
         QueueUrl="https://sqs.us-east-1.amazonaws.com/387343799327/development-enchanted-brain-callback-{}".format(
             connection_id[:-1]
