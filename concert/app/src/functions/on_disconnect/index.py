ad2bd6bd0e6d1789faf137a64fa01ef193a08a7c
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-03 00:57:05 +0200

    Clean up subscriptions and event source mappings

--- concert/app/src/functions/on_disconnect/index.py
+++ concert/app/src/functions/on_disconnect/index.py
@@ -2,17 +2,22 @@ import boto3
 import os
 
 
+TABLE_NAME = os.environ.get("TABLE_NAME")
+
+client_lambda = boto3.client("lambda")
+client_sns = boto3.client("sns")
 client_sqs = boto3.client("sqs")
+
+dynamodb = boto3.resource("dynamodb")
+table = boto3.table(TABLE_NAME)
 
 
 def handler(event, context):
     connection_id = event["requestContext"]["connectionId"]
+    resources = table.get_item(Key={"userId": "CONN${}".format(connection_id)})["Item"]
 
-    # TODO: Read queue URL, event source mapping UUID, and subscription ARN
-    client_sqs.delete_queue(
-        QueueUrl="https://sqs.us-east-1.amazonaws.com/387343799327/development-enchanted-brain-callback-{}".format(
-            connection_id[:-1]
-        )
-    )
+    client_sns.unsubscribe(SubscriptionArn=resources["subscription_arn"])
+    client_lambda.delete_event_source_mapping(UUID=resources["mapping_uuid"])
+    client_sqs.delete_queue(QueueUrl=resources["queue_url"])
 
     return {"statusCode": 204}
