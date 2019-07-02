2ef0a68df55765060ce69162dc461ae9fd34ce2d
borchardt <jackson.borchardt@gmail.com>
2019-07-02 12:41:53 -0700

    on_choice_made: print websocket data, don't publish to sns

--- concert/app/src/functions/on_choice_made/index.py
+++ concert/app/src/functions/on_choice_made/index.py
@@ -1,7 +1,27 @@
+import boto3
+import json
 import os
+# from enchanted_brain.attributes import ATTR_USER_ID
 
+"""
+Processes websocket API messages from API Gateway
+"""
+
+# SNS_CHOICE_MADE_ARN = os.environ["CHOICE_MADE_SNS_TOPIC_ARN"]
+
+sns = boto3.client("sns")
 
 def handler(event, context):
+    # print(ATTR_USER_ID)
     print(event)
     print(os.environ)
+
+    data = event["data"]
+    print(data)
+    # response = sns.publish(
+    #     TopicArn=SNS_CHOICE_MADE_ARN,
+    #     Message=json.dumps(data),
+    #     MessageStructure="string",
+    # )
+
     return {"statusCode": 204}
