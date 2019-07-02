2aba0ab53b2122a62e7d8d181207317c5c3b92db
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2019-07-02 13:22:28 -0700

    Setup amplify user sign in.

--- client/app/App.js
+++ client/app/App.js
@@ -1,48 +1,117 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
-import EStyleSheet from 'react-native-extended-stylesheet';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import AppNavigator from "./navigation/AppNavigator";
+import layout from "./constants/Layout";
 
-import layout from './constants/Layout';
+Amplify.configure({
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_mbDYzVglq",
+    userPoolWebClientId: "5jppn76b0lkv0cnmmhtnk4gpvr"
+  }
+});
+
+const signUpConfig = {
+  header: "Sign Up",
+  hideAllDefaults: true,
+  defaultCountryCode: "41",
+  usernameAttributes: "email",
+  signUpFields: [
+    {
+      label: "Name",
+      key: "name",
+      required: true,
+      displayOrder: 1,
+      type: "string"
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 3,
+      type: "string"
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 3,
+      type: "password"
+    },
+    {
+      label: "Phone",
+      key: "phone_number",
+      required: true,
+      displayOrder: 4,
+      type: "string"
+    },
+    {
+      label: "gender",
+      key: "gender",
+      required: true,
+      displayOrder: 5,
+      type: "string",
+      custom: true
+    },
+    {
+      label: "Age",
+      key: "age",
+      required: true,
+      displayOrder: 6,
+      type: "number",
+      custom: true
+    },
+    {
+      label: "Color-perception",
+      key: "color-perception",
+      required: true,
+      displayOrder: 7,
+      type: "string",
+      custom: true
+    }
+  ]
+};
 
 const SCREEN_WIDTH = layout.window.width;
 
 // Abitrary min, max for rem and width
-const MIN_WIDTH = 300
-const MAX_WIDTH = 900
-const MIN_REM = 14
-const MAX_REM = 36
+const MIN_WIDTH = 300;
+const MAX_WIDTH = 900;
+const MIN_REM = 14;
+const MAX_REM = 36;
 
 // returns a rem value based on the screen width, within max and min screen and rem bounds
 const calculateRem = (width, minWidth, maxWidth, minRem, maxRem) => {
   if (width <= minWidth) {
-    return minRem
+    return minRem;
   }
   if (width >= maxWidth) {
-    return maxRem
+    return maxRem;
   }
   // get the difference in min and max rem values
-  const remDiff = maxRem - minRem
+  const remDiff = maxRem - minRem;
 
   // get the difference in min and max widths
-  const widthRange = maxWidth - minWidth
+  const widthRange = maxWidth - minWidth;
 
   // get the difference between the actual screen's width and the min width
-  const widthDiff = width - minWidth
+  const widthDiff = width - minWidth;
 
   // the rem scaling factor is based on the proportion of width range
   // the actual screen width takes up
-  const remFactor = widthDiff / widthRange
+  const remFactor = widthDiff / widthRange;
 
   // the calculated rem is the min rem plus the difference between
   // min and max rem times the proportion of the width range
   // the actual screen width takes up
-  const calculatedRem = minRem + (remDiff * remFactor)
+  const calculatedRem = minRem + remDiff * remFactor;
 
-  return calculatedRem
-}
+  return calculatedRem;
+};
 
 EStyleSheet.build({
   $rem: calculateRem(SCREEN_WIDTH, MIN_WIDTH, MAX_WIDTH, MIN_REM, MAX_REM)
@@ -52,67 +121,30 @@ const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'tomato',
-    accent: 'yellow',
-  },
+    primary: "tomato",
+    accent: "yellow"
+  }
 };
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
+class App extends React.Component {
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <PaperProvider theme={theme}>
-          <View style={styles.container}>
-            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-            <AppNavigator />
-          </View>
-        </PaperProvider>
-      );
-    }
+    console.log("Rendering app....");
+    return (
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </PaperProvider>
+    );
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
+
+export default withAuthenticator(App, { signUpConfig });
