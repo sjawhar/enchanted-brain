f7c066bf1e2f1cd0e809ae997c6c05fc6d7b72c0
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-03 00:21:12 +0200

    Delete queue on disconnect

--- concert/app/src/functions/on_disconnect/index.py
+++ concert/app/src/functions/on_disconnect/index.py
@@ -1,7 +1,22 @@
 import os
 
 
+CALLBACK_FUNCTION_ARN = os.environ.get("CALLBACK_FUNCTION_ARN")
+CALLBACK_SNS_TOPIC_ARN = os.environ.get("CALLBACK_SNS_TOPIC_ARN")
+CALLBACK_SQS_QUEUE_ARN_PREFIX = os.environ.get("CALLBACK_SQS_QUEUE_ARN_PREFIX")
+
+client_lambda = boto3.client("lambda")
+client_sns = boto3.client("sns")
+client_sqs = boto3.client("sqs")
+
+
 def handler(event, context):
-    print(event)
-    print(os.environ)
+    connection_id = event["requestContext"]["connectionId"]
+
+    client_sqs.delete_queue(
+        QueueUrl="https://sqs.us-east-1.amazonaws.com/387343799327/development-enchanted-brain-callback-{}".format(
+            connection_id[:-1]
+        )
+    )
+
     return {"statusCode": 204}
