d5f31d826d633982d573b39ebb6587b952a2521e
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-03 01:33:59 +0200

    Disable event source mapping before deleting

--- concert/app/src/functions/on_disconnect/index.py
+++ concert/app/src/functions/on_disconnect/index.py
@@ -16,6 +16,9 @@ def handler(event, context):
     connection_id = event["requestContext"]["connectionId"]
     resources = table.get_item(Key={"userId": "CONN${}".format(connection_id)})["Item"]
 
+    client_lambda.update_event_source_mapping(
+        UUID=resources["mapping_uuid"], Enabled=False
+    )
     client_sns.unsubscribe(SubscriptionArn=resources["subscription_arn"])
     client_sqs.delete_queue(QueueUrl=resources["queue_url"])
     client_lambda.delete_event_source_mapping(UUID=resources["mapping_uuid"])
