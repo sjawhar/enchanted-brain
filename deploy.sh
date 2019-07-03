f18a2e07887482c21f3ffece98eb0d1969385a75
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-03 09:53:44 +0200

    Updated deploy process for lambda vendor packages

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,28 @@ fi
 S3_BUCKET="thecybermonk-cloudformation-artifacts"
 OUTPUT_TEMPLATE="cloudformation-generated.yml"
 
+for PACKAGE_DIR in $(find concert/app/src \
+  -type d -name lib -prune \
+  -o -type f -name Pipfile \
+  -exec dirname {} \;
+)
+do
+  pushd "${PACKAGE_DIR}"
+  PACKAGE_NAME="$(basename "${PACKAGE_DIR}")"
+
+  rm -rf vendor
+  mkdir vendor
+
+  pipenv lock -r > requirements.txt
+  pip install --target vendor -r requirements.txt
+  rm requirements.txt
+
+  mv Pipfile "../Pipfile.${PACKAGE_NAME}.bak"
+  mv Pipfile.lock "../Pipfile.${PACKAGE_NAME}.lock.bak"
+
+  popd
+done
+
 echo "Packaging..."
 aws cloudformation package \
   --template-file cloudformation.yml \
@@ -24,6 +46,20 @@ aws cloudformation package \
   --s3-bucket $S3_BUCKET \
   --s3-prefix "${ENVIRONMENT}/enchanted-brain" \
   $REGION_FLAG
+
+for PACKAGE_FILE in $(find concert/app/src \
+  -type d -name vendor -prune \
+  -o -type f -name 'Pipfile.*.lock.bak' \
+  -print
+)
+do
+  pushd "$(dirname "${PACKAGE_FILE}")"
+  PACKAGE_NAME="$(basename "${PACKAGE_FILE}" | awk -F '.' '{ print $2; }')"
+
+  mv "Pipfile.${PACKAGE_NAME}.bak" "./${PACKAGE_NAME}/Pipfile"
+  mv "Pipfile.${PACKAGE_NAME}.lock.bak" "./${PACKAGE_NAME}/Pipfile.lock"
+  popd
+done
 
 echo "Deploying..."
 aws cloudformation deploy \
