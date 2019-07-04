5f4759b183d63ecd68733f969fd53b1a3171f4e9
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-04 09:46:37 +0200

    Revert to direct resource creation on dis/connect

--- concert/app/src/functions/on_disconnect/index.py
+++ concert/app/src/functions/on_disconnect/index.py
@@ -1,7 +1,9 @@
 import boto3
 import os
 from enchanted_brain.attributes import (
-    ATTR_CONNECTION_STACK_NAME,
+    ATTR_CONNECTION_LAMBDA_MAPPING_UUID,
+    ATTR_CONNECTION_SNS_SUBSCRIPTION_ARN,
+    ATTR_CONNECTION_SQS_QUEUE_URL,
     ATTR_RECORD_ID,
     ATTR_RECORD_TYPE,
     RECORD_TYPE_CONNECTION,
@@ -10,7 +12,9 @@ from enchanted_brain.attributes import (
 
 DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
 
-cloudformation = boto3.client("cloudformation")
+client_lambda = boto3.client("lambda")
+client_sns = boto3.client("sns")
+client_sqs = boto3.client("sqs")
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(DYNAMODB_TABLE_NAME)
@@ -22,8 +26,23 @@ def handler(event, context):
         ATTR_RECORD_ID: event["requestContext"]["connectionId"],
     }
 
-    stack_name = table.get_item(Key=connection_key)["Item"][ATTR_CONNECTION_STACK_NAME]
-    cloudformation.delete_stack(StackName=stack_name)
+    resources = table.get_item(Key=connection_key)["Item"]
+
+    client_sns.unsubscribe(
+        SubscriptionArn=resources[ATTR_CONNECTION_SNS_SUBSCRIPTION_ARN]
+    )
+    client_sqs.delete_queue(QueueUrl=resources[ATTR_CONNECTION_SQS_QUEUE_URL])
+
+    for i in range(5):
+        try:
+            client_lambda.delete_event_source_mapping(
+                UUID=resources[ATTR_CONNECTION_LAMBDA_MAPPING_UUID]
+            )
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(2 ** i)
+
     table.delete_item(Key=connection_key)
 
     return {"statusCode": 204}
