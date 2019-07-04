9d7f74ea981cb930a78f90432cef9a01227e6886
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-04 09:46:37 +0200

    Revert to direct resource creation on dis/connect

--- concert/app/src/functions/on_connect/index.py
+++ concert/app/src/functions/on_connect/index.py
@@ -1,47 +1,75 @@
 import boto3
 import os
+import json
 from enchanted_brain.attributes import (
-    ATTR_CONNECTION_STACK_NAME,
+    ATTR_CONNECTION_LAMBDA_MAPPING_UUID,
+    ATTR_CONNECTION_SNS_SUBSCRIPTION_ARN,
+    ATTR_CONNECTION_SQS_QUEUE_URL,
     ATTR_RECORD_ID,
     ATTR_RECORD_TYPE,
     RECORD_TYPE_CONNECTION,
 )
 
 
-APP_ENVIRONMENT = os.environ.get("APP_ENVIRONMENT")
-CLOUDFORMATION_STACK_NAME_PREFIX = os.environ.get("CLOUDFORMATION_STACK_NAME_PREFIX")
+CALLBACK_FUNCTION_ARN = os.environ.get("CALLBACK_FUNCTION_ARN")
+CALLBACK_SNS_TOPIC_ARN = os.environ.get("CALLBACK_SNS_TOPIC_ARN")
+CALLBACK_SQS_QUEUE_ARN_PREFIX = os.environ.get("CALLBACK_SQS_QUEUE_ARN_PREFIX")
 DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
 
-cloudformation = boto3.client("cloudformation")
+client_lambda = boto3.client("lambda")
+client_sns = boto3.client("sns")
+client_sqs = boto3.client("sqs")
+
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(DYNAMODB_TABLE_NAME)
-
-CLOUDFORMATION_STACK_TEMPLATE_BODY = ""
-CLOUDFORMATION_STACK_TAGS = [
-    {"Key": "environment", "Value": APP_ENVIRONMENT},
-    {"Key": "system", "Value": "enchanted-brain"},
-    {"Key": "component", "Value": "callback"},
-]
-
-with open("cloudformation.yml", "r") as file:
-    CLOUDFORMATION_STACK_TEMPLATE_BODY = file.read()
 
 
 def handler(event, context):
     connection_id = event["requestContext"]["connectionId"]
-    stack_name = "-".join([CLOUDFORMATION_STACK_NAME_PREFIX, connection_id[:-1]])
+    queue_arn = "-".join([CALLBACK_SQS_QUEUE_ARN_PREFIX, connection_id[:-1]])
+    queue_url = client_sqs.create_queue(
+        QueueName=queue_arn.split(":")[-1],
+        Attributes={
+            "Policy": json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Action": "sqs:SendMessage",
+                            "Resource": queue_arn,
+                            "Principal": "*",
+                            "Condition": {
+                                "ArnEquals": {"aws:SourceArn": CALLBACK_SNS_TOPIC_ARN}
+                            },
+                        }
+                    ],
+                }
+            )
+        },
+    )["QueueUrl"]
+    mapping_uuid = client_lambda.create_event_source_mapping(
+        EventSourceArn=queue_arn,
+        FunctionName=CALLBACK_FUNCTION_ARN,
+        Enabled=True,
+        BatchSize=1,
+    )["UUID"]
+    subscription_arn = client_sns.subscribe(
+        TopicArn=CALLBACK_SNS_TOPIC_ARN,
+        Protocol="sqs",
+        Endpoint=queue_arn,
+        Attributes={"RawMessageDelivery": "true"},
+        ReturnSubscriptionArn=True,
+    )["SubscriptionArn"]
+
     table.put_item(
         Item={
             ATTR_RECORD_TYPE: RECORD_TYPE_CONNECTION,
             ATTR_RECORD_ID: connection_id,
-            ATTR_CONNECTION_STACK_NAME: stack_name,
+            ATTR_CONNECTION_LAMBDA_MAPPING_UUID: mapping_uuid,
+            ATTR_CONNECTION_SNS_SUBSCRIPTION_ARN: subscription_arn,
+            ATTR_CONNECTION_SQS_QUEUE_URL: queue_url,
         }
     )
-    stack_id = cloudformation.create_stack(
-        StackName=stack_name,
-        TemplateBody=CLOUDFORMATION_STACK_TEMPLATE_BODY,
-        Parameters=[{"ParameterKey": "Environment", "ParameterValue": APP_ENVIRONMENT}],
-        Tags=CLOUDFORMATION_STACK_TAGS,
-    )["StackId"]
 
     return {"statusCode": 204}
