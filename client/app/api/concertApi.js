d6f833bcaac101c18b20c010749732f40ec5ea54
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-05 23:13:47 +0200

    Added connect/disconnect logic to concertApi module

--- client/app/api/concertApi.js
+++ client/app/api/concertApi.js
@@ -1,31 +1,65 @@
 import EventEmitter from "events";
 
-const ws = new WebSocket(
-  "wss://gem4rt4c0j.execute-api.us-east-1.amazonaws.com/Prod"
-);
+let ws = null;
+let isConnect = false;
+const events = new EventEmitter();
 
-ws.onopen = () => {
-  console.log("CONNECTED");
-  ws.send(
-    JSON.stringify({
-      event: "CHOICE_MADE",
-      choiceType: "CHOICE_COLOR",
-      color: "COLOR_BLUE"
-    })
+const connect = idToken => {
+  if (ws) {
+    return;
+  }
+
+  isConnect = true;
+  ws = new WebSocket(
+    "wss://gem4rt4c0j.execute-api.us-east-1.amazonaws.com/Prod",
+    null,
+    {
+      headers: { Authorization: idToken },
+    },
   );
-  console.log("MESSAGE SENT.");
+
+  ws.onopen = () => {
+    console.log("CONNECTED");
+    ws.send(
+      JSON.stringify({
+        event: "CHOICE_MADE",
+        choiceType: "CHOICE_COLOR",
+        color: "COLOR_BLUE"
+      })
+    );
+    console.log("MESSAGE SENT.");
+  };
+  ws.onmessage = message => {
+    console.log("MESSAGE", message.data);
+    try {
+      const { event, data } = JSON.parse(message.data);
+      if (event === 'EVENT_STAGE_CHANGED') {
+        events.emit('SHOW_COLOR_PICKER');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  ws.onerror = e => {
+    console.error("ERROR", e.message);
+  };
+
+  ws.onclose = e => {
+    ws = null;
+    console.log("CLOSED", e.code, e.reason);
+    if (isConnect) {
+      connect(idToken);
+    }
+  };
 };
 
-ws.onmessage = e => {
-  console.log("MESSAGE", e.data);
-};
+const disconnect = () => {
+  isConnect = false;
+  if (!ws) {
+    return;
+  }
+  ws.close();
+}
 
-ws.onerror = e => {
-  console.error("ERROR", e.message);
-};
-
-ws.onclose = e => {
-  console.log("CLOSED", e.code, e.reason);
-};
-
-export default new EventEmitter();
+export default Object.assign(events, { connect, disconnect });
