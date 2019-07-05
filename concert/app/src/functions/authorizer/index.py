2f67ed777a3229d30538bd9c9099effdfc208fe3
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-05 22:42:28 +0200

    Implemented prototype authorizer

--- concert/app/src/functions/authorizer/index.py
+++ concert/app/src/functions/authorizer/index.py
@@ -1,8 +1,27 @@
+import base64
+import json
 import os
+
+API_ARN = os.environ.get("API_ARN")
+COGNITO_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
+COGNITO_ADMIN_GROUP_NAME = os.environ.get("COGNITO_ADMIN_GROUP_NAME")
 
 
 def handler(event, context):
-    print(event)
-    print(context)
-    print(os.environ)
-    return {"statusCode": 204}
+    token = event["headers"]["Authorization"]
+    # payload = json.loads(base64.decode(token.split('.')[1]))
+
+    return {
+        "principalId": token,
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "execute-api:Invoke",
+                    "Resource": "{}/*".format(API_ARN),
+                }
+            ],
+        },
+        "context": {"hello-person": "stringval"},
+    }
