8099f0720706bd79f36377c74b4f8a8df7ac10d5
borchardt <jackson.borchardt@gmail.com>
2019-07-05 21:58:18 -0700

    on-choice-made publushes to choice-made sns topic

--- concert/app/src/functions/on_choice_made/index.py
+++ concert/app/src/functions/on_choice_made/index.py
@@ -8,22 +8,16 @@ import os
 Processes websocket API messages from API Gateway
 """
 
-# SNS_CHOICE_MADE_ARN = os.environ["CHOICE_MADE_SNS_TOPIC_ARN"]
+SNS_CHOICE_MADE_ARN = os.environ["CHOICE_MADE_SNS_TOPIC_ARN"]
 
 sns = boto3.client("sns")
 
 
 def handler(event, context):
     # print(ATTR_USER_ID)
-    print(event)
-    print(os.environ)
 
-    # data = event["data"]
-    # print(data)
-    # response = sns.publish(
-    #     TopicArn=SNS_CHOICE_MADE_ARN,
-    #     Message=json.dumps(data),
-    #     MessageStructure="string",
-    # )
-
+    message = event["body"]
+    response = sns.publish(
+        TopicArn=SNS_CHOICE_MADE_ARN, Message=message, MessageStructure="string"
+    )
     return {"body": "this is a response"}
