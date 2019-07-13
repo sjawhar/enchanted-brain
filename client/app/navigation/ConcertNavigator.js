fc98c8c49a67544e971a65647cd2e60050a27805
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2019-07-13 13:05:57 -0700

    Convert to switch nav. Add all concert screens.

--- client/app/navigation/ConcertNavigator.js
+++ client/app/navigation/ConcertNavigator.js
@@ -1,16 +1,19 @@
-import { createStackNavigator } from "react-navigation";
+import { createSwitchNavigator } from "react-navigation";
 
+import ChillsScreen from "../screens/Chills";
 import ColorPickerScreen from "../screens/ColorPicker";
+import EmotionPicker from "../screens/EmotionPicker";
+import MentalImageryScreen from "../screens/MentalImagery";
+import ResultsScreen from "../screens/Results";
 import WelcomeScreen from "../screens/Welcome";
 
-const ConcertNavigator = createStackNavigator(
-  {
-    Welcome: WelcomeScreen,
-    Colors: ColorPickerScreen
-  },
-  {
-    headerMode: "none"
-  }
-);
+const ConcertNavigator = createSwitchNavigator({
+  Welcome: WelcomeScreen,
+  MentalImagery: MentalImageryScreen,
+  Colors: ColorPickerScreen,
+  Emotions: EmotionPicker,
+  Chills: ChillsScreen,
+  Results: ResultsScreen
+});
 
 export default ConcertNavigator;
