dc6be4c0175464e6a91615c8da2920c9ab34ca33
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-15 17:39:58 +0200

    Use fixtures for tests, fix dbsetup and event.sns.dist

--- concert/app/test/__init__.py
+++ concert/app/test/__init__.py
@@ -1,6 +1,6 @@
 import os
 
 # setup dummy environment variables
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 os.environ["EVENT_NAME"] = "CONCERT_LAUSANNE_2019"
-os.environ["DYNAMODB_REGION"] = "us-west-2"
-os.environ["DYNAMODB_TABLE_NAME"] = "choices table"
+os.environ["DYNAMODB_TABLE_NAME"] = "testing-enchanted-brain"
