4871592abf38c8d3f74a1e34aa4f126e1a18c5f8
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-15 17:52:42 +0200

    Fixed eslintrc format

--- client/app/.eslintrc.js
+++ client/app/.eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   extends: "universe/native",
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   globals: {
-    "WebSocket": "readonly",
+    WebSocket: "readonly",
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: "detect",
+    },
+  },
 };
