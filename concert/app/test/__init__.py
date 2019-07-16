4d072db30166e201d4b37a3942533325ac02e02e
borchardt <jackson.borchardt@gmail.com>
2019-07-16 00:32:11 -0700

    add tests for callback function

--- concert/app/test/__init__.py
+++ concert/app/test/__init__.py
@@ -4,3 +4,4 @@ import os
 os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 os.environ["EVENT_NAME"] = "CONCERT_LAUSANNE_2019"
 os.environ["DYNAMODB_TABLE_NAME"] = "testing-enchanted-brain"
+os.environ["API_URL"] = "https://abc.execute-api.us-east-1.amazonaws.com/Prod"
