c10b66468aa25787e2b53c01529bfadb8a7ad33e
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2019-07-17 15:43:59 -0700

    Upgrade to Expo SDK 33.

--- client/app/src/screens/EmotionPicker.js
+++ client/app/src/screens/EmotionPicker.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { Constants } from 'expo';
+import Constants from 'expo-constants';
 
 import EmotionButton from '../features/emotions/EmotionButton';
 import Layout from '../constants/Layout';
