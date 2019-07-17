22a2e6647a1cc9b36289ee39be2e4125d0972031
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2019-07-17 16:32:07 -0700

    Add mental imagery and thank you screens to nav.

--- client/app/src/navigation/ConcertNavigator.js
+++ client/app/src/navigation/ConcertNavigator.js
@@ -1,11 +1,15 @@
 import { createStackNavigator } from 'react-navigation';
 
 import ColorPickerScreen from '../screens/ColorPicker';
+import MentalImageryScreen from '../screens/MentalImagery';
+import ThankYouScreen from '../screens/SurveyThankYou';
 import WelcomeScreen from '../screens/Welcome';
 
 const ConcertNavigator = createStackNavigator(
   {
     Welcome: WelcomeScreen,
+    MentalImagery: MentalImageryScreen,
+    SurveyThankYou: ThankYouScreen,
     Colors: ColorPickerScreen,
   },
   {
