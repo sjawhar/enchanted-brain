8f3c7b491d6d0bc4caa002882a4252fc1eff7bf3
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-20 20:20:01 +0200

    Added tests for authorizer and finished functionality

--- concert/app/src/functions/authorizer/index.py
+++ concert/app/src/functions/authorizer/index.py
@@ -1,36 +1,92 @@
 import base64
+import hashlib
 import json
 import os
+import time
+from jose import jwk, jwt
+from jose.utils import base64url_decode
+from urllib.request import urlopen
 
 API_ARN = os.environ.get("API_ARN")
+API_METHOD_CHOICE_MADE = os.environ.get("API_METHOD_CHOICE_MADE")
+API_METHOD_EVENT_STAGE_CHANGED = os.environ.get("API_METHOD_EVENT_STAGE_CHANGED")
+COGNITO_GROUP_ADMIN = os.environ.get("COGNITO_GROUP_ADMIN")
+COGNITO_GROUP_VISUALIZATION = os.environ.get("COGNITO_GROUP_VISUALIZATION")
 COGNITO_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
-COGNITO_ADMIN_GROUP_NAME = os.environ.get("COGNITO_ADMIN_GROUP_NAME")
+COGNITO_REGION = COGNITO_USER_POOL_ID.split("_")[0]
 
-# TODO:
-# Verify JWT
-# Check that user is in the concert group
-# Write method-specific policy based on user groups (admin, vis, etc.)
+response = urlopen(
+    "https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json".format(
+        COGNITO_REGION, COGNITO_USER_POOL_ID
+    )
+)
+JWKS_KEYS = json.loads(response.read())["keys"]
+
+CHOICE_BUCKETS = [
+    "CHOICE_COLOR",
+    "CHOICE_COLOR",
+    "CHOICE_EMOTION_HAPPINESS",
+    "CHOICE_EMOTION_AGITATION",
+]
+NUM_BUCKETS = len(CHOICE_BUCKETS)
+
+
+def get_verified_token(token):
+    headers = jwt.get_unverified_headers(token)
+    kid = headers["kid"]
+
+    key_index = -1
+    for i in range(len(JWKS_KEYS)):
+        if kid == JWKS_KEYS[i]["kid"]:
+            key_index = i
+            break
+    if key_index == -1:
+        return None
+
+    return jwt.decode(token, JWKS_KEYS[key_index])
+
+
+def get_user_context(user_id):
+    user_id_hash = int(hashlib.md5(user_id.encode("utf-8")).hexdigest(), 16)
+    choice_mod = user_id_hash % (NUM_BUCKETS * 2)
+    return {
+        "choiceType": CHOICE_BUCKETS[int(choice_mod / 2)],
+        "choiceInverted": bool(choice_mod % 2),
+    }
 
 
 def handler(event, context):
-    token = event["headers"]["Authorization"]
-    payload = {"sub": token}
-
-    try:
-        payload = json.loads(base64.decode(token.split(".")[1]))
-    except Exception as e:
-        print(e)
-
-    return {
-        "principalId": payload["sub"],
+    policy = {
+        "principalId": "Unauthorized",
         "policyDocument": {
             "Version": "2012-10-17",
             "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": "execute-api:Invoke",
-                    "Resource": "{}/*".format(API_ARN),
-                }
+                {"Effect": "Deny", "Action": "execute-api:Invoke", "Resource": "*"}
             ],
         },
     }
+
+    try:
+        token = get_verified_token(event["headers"]["Authorization"])
+        if not token:
+            raise Exception("Token not verified")
+    except Exception as e:
+        print(e)
+        return policy
+
+    policy["principalId"] = token["sub"]
+    statement = policy["policyDocument"]["Statement"][0]
+    statement["Effect"] = "Allow"
+    statement["Resource"] = [
+        "{}/{}".format(API_ARN, method) for method in ["$connect", "$disconnect"]
+    ]
+    groups = token["cognito:groups"] if "cognito:groups" in token else []
+    if COGNITO_GROUP_ADMIN in groups:
+        statement["Resource"].append(
+            "{}/{}".format(API_ARN, API_METHOD_EVENT_STAGE_CHANGED)
+        )
+    elif COGNITO_GROUP_VISUALIZATION not in groups:
+        # User is an audience member
+        statement["Resource"].append("{}/{}".format(API_ARN, API_METHOD_CHOICE_MADE))
+        policy["context"] = get_user_context(policy["principalId"])
+    return policy
