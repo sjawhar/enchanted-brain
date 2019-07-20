6a3faffb3287dd5a8d52b7eb14fc6b7de42b8589
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2019-07-20 12:13:15 -0700

    Remove last known screen from redux state.

--- client/app/src/state/actionTypes.js
+++ client/app/src/state/actionTypes.js
@@ -1,5 +1,4 @@
-const SET_CHOICE_TYPE = "set_choice_type";
-const SET_CHOICE_INVERTED = "set_choice_inverted";
-const SET_LAST_KNOWN_STAGE = "set_last_known_stage";
+const SET_CHOICE_TYPE = 'set_choice_type';
+const SET_CHOICE_INVERTED = 'set_choice_inverted';
 
-export default { SET_CHOICE_TYPE, SET_CHOICE_INVERTED, SET_LAST_KNOWN_STAGE };
+export default { SET_CHOICE_TYPE, SET_CHOICE_INVERTED };
--- client/app/src/state/reducer.js
+++ client/app/src/state/reducer.js
@@ -1,17 +1,13 @@
-import actionTypes from "./actionTypes";
+import actionTypes from './actionTypes';
 
 const INITIAL_STATE = {
-  lastKnownScreen: "Welcome",
-  choiceType: "",
-  choiceInverted: null
+  choiceType: '',
+  choiceInverted: null,
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
   const { type } = action;
   switch (type) {
-    case actionTypes.SET_LAST_KNOWN_STAGE: {
-      return { ...state, lastKnownScreen: action.payload.screen };
-    }
     case actionTypes.SET_CHOICE_TYPE: {
       return { ...state, choiceType: action.payload.choiceType };
     }
