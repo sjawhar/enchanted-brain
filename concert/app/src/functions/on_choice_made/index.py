48e4c5e4ba9f22883ae07145de3b9ce6715754d7
borchardt <jackson.borchardt@gmail.com>
2019-07-23 04:15:48 -0700

    Add CloudFormation resources for Kinesis delivery stream and analytics ap. OnChoiceMade function publishes to delivery stream.

--- concert/app/src/functions/on_choice_made/index.py
+++ concert/app/src/functions/on_choice_made/index.py
@@ -7,8 +7,10 @@ Processes websocket API messages from API Gateway
 """
 
 SNS_CHOICE_MADE_ARN = os.environ["CHOICE_MADE_SNS_TOPIC_ARN"]
+AGGREGATE_CHOICE_DELIVERY_STREAM_NAME = os.environ["AGGREGATE_CHOICE_DELIVERY_STREAM_NAME"]
 
 sns = boto3.client("sns")
+firehose = boto3.client("firehose")
 
 
 def handler(event, context):
@@ -16,10 +18,14 @@ def handler(event, context):
     message = json.loads(event["body"])["data"]
     message["userId"] = event["requestContext"]["authorizer"]["principalId"]
 
-    response = sns.publish(
+    sns_response = sns.publish(
         TopicArn=SNS_CHOICE_MADE_ARN,
         Message=json.dumps(message),
         MessageStructure="string",
+    )
+    firehose_response = firehose.put_record(
+        DeliveryStreamName=AGGREGATE_CHOICE_DELIVERY_STREAM_NAME,
+        Record={"Data": json.dumps(message)}
     )
 
     return {"statusCode": 204}
