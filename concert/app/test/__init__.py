7204feac3e8c3916ced0cd21469831b9aed18efb
borchardt <jackson.borchardt@gmail.com>
2019-07-25 01:02:17 -0700

    move test_callback and test_choice_writer to individual directories, assert expected calls made by callback

--- concert/app/test/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# setup dummy environment variables
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
-os.environ["EVENT_NAME"] = "CONCERT_LAUSANNE_2019"
-os.environ["DYNAMODB_TABLE_NAME"] = "testing-enchanted-brain"
-os.environ["API_URL"] = "https://abc.execute-api.us-east-1.amazonaws.com/Prod"
