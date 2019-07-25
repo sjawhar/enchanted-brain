c3ea7823ca8093b9e61f1a299f5ea432d92bf7de
borchardt <jackson.borchardt@gmail.com>
2019-07-24 21:11:54 -0700

    dummy environment variables for on_choice_made tests

---
+++ concert/app/test/on_choice_made/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+# setup dummy environment variables
+os.environ["CHOICE_MADE_SNS_TOPIC_ARN"] = "choice made topic"
+os.environ["AGGREGATE_CHOICE_DELIVERY_STREAM_NAME"] = "aggregate choice stream"
