09f47c1a5bd8b99892c490f7746be8c1704357d8
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-25 19:19:45 +0200

    Reorganize test init files

---
+++ concert/app/test/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["EVENT_NAME"] = "CONCERT_LAUSANNE_2019"
--- concert/app/test/callback/__init__.py
+++ concert/app/test/callback/__init__.py
@@ -1,5 +1,3 @@
 import os
 
-# setup dummy environment variables
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 os.environ["API_URL"] = "https://abc.execute-api.us-east-1.amazonaws.com/Prod"
