9fbd2e4b3a623962efc46810d8b91109d670e554
borchardt <jackson.borchardt@gmail.com>
2019-07-27 04:48:41 -0700

    on_choice_made publishes to visualization topic

--- concert/app/src/functions/on_choice_made/index.py
+++ concert/app/src/functions/on_choice_made/index.py
@@ -6,10 +6,11 @@ import os
 Processes websocket API messages from API Gateway
 """
 
-AGGREGATE_CHOICE_DELIVERY_STREAM_NAME = os.environ[
+AGGREGATE_CHOICE_DELIVERY_STREAM_NAME = os.environ.get(
     "AGGREGATE_CHOICE_DELIVERY_STREAM_NAME"
-]
-CHOICE_MADE_SNS_TOPIC_ARN = os.environ["CHOICE_MADE_SNS_TOPIC_ARN"]
+)
+CHOICE_MADE_SNS_TOPIC_ARN = os.environ.get("CHOICE_MADE_SNS_TOPIC_ARN")
+CALLBACK_VISUALIZATION_SNS_TOPIC_ARN = os.environ.get("CALLBACK_VISUALIZATION_SNS_TOPIC_ARN")
 
 sns = boto3.client("sns")
 firehose = boto3.client("firehose")
@@ -20,8 +21,13 @@ def handler(event, context):
     message = json.loads(event["body"])["data"]
     message["userId"] = event["requestContext"]["authorizer"]["principalId"]
 
-    response = sns.publish(
+    choice_made_sns_response = sns.publish(
         TopicArn=CHOICE_MADE_SNS_TOPIC_ARN,
+        Message=json.dumps(message),
+        MessageStructure="string",
+    )
+    visualization_sns_response = sns.publish(
+        TopicArn=CALLBACK_VISUALIZATION_SNS_TOPIC_ARN,
         Message=json.dumps(message),
         MessageStructure="string",
     )
