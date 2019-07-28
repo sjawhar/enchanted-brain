58e91d31bb52a8ac513870ab954e7b4aaef28efd
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-28 12:01:51 +0200

    Implemented color value names

--- client/app/src/constants/Colors.js
+++ client/app/src/constants/Colors.js
@@ -1,17 +1,22 @@
 import Color from 'color';
 const tintColor = '#2f95dc';
 
+const COLOR_SUFFIX = ['LIGHT', 'MEDIUM', 'DARK'];
+
 export const swatchColors = [
-  [[255, 0, 0], 0.5, 0.33],
-  [[255, 128, 0], 0.5, 0.3],
-  [[255, 255, 0], 0.5, 0.3],
-  [[0, 255, 0], 0.5, 0.4],
-  [[0, 0, 255], 0.5, 0.33],
-  [[255, 0, 255], 0.5, 0.33],
-  [[128, 128, 128], 1],
-].map(([rgb, lighten, darken]) =>
+  ['RED', [255, 0, 0], 0.5, 0.33],
+  ['ORANGE', [255, 128, 0], 0.5, 0.3],
+  ['YELLOW', [255, 255, 0], 0.5, 0.3],
+  ['GREEN', [0, 255, 0], 0.5, 0.4],
+  ['BLUE', [0, 0, 255], 0.5, 0.33],
+  ['PURPLE', [255, 0, 255], 0.5, 0.33],
+  ['NEUTRAL', [128, 128, 128], 1],
+].map(([colorType, rgb, lighten, darken]) =>
   [Color.rgb(rgb).lighten(lighten), Color.rgb(rgb), Color.rgb(rgb).darken(darken || lighten)].map(
-    color => color.rgb().string()
+    (color, index) => ({
+      id: `COLOR_${colorType}_${COLOR_SUFFIX[index]}`,
+      value: color.rgb().string(),
+    })
   )
 );
 
