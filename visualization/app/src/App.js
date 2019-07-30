23bb5f6970d659591069a0783f91558f8141e698
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-07-30 14:47:27 +0200

    Implemented listen flag

--- visualization/app/src/App.js
+++ visualization/app/src/App.js
@@ -4,47 +4,73 @@ import { withAuthenticator } from 'aws-amplify-react';
 
 import concertApi from './util/concertApi';
 import amplifyConfig from './config/amplify';
+import CHOICE_TYPES, {
+  CHOICE_COLOR,
+  CHOICE_EMOTION_ENERGY,
+  CHOICE_EMOTION_HAPPINESS
+} from './constants/ChoiceTypes';
 
 Amplify.configure(amplifyConfig);
 
-const BUFFER_RING_SIZE = 50;
+const BUFFER_RING_SIZE = parseInt(process.env.REACT_APP_BUFFER_RING_SIZE, 10);
 
 class App extends Component {
   constructor(props) {
     super(props);
-    const state = {
-      CHOICE_EMOTION_HAPPINESS: {
-        buffer: Array(BUFFER_RING_SIZE),
-        index: 0
-      },
-      CHOICE_EMOTION_ENERGY: {
-        buffer: Array(BUFFER_RING_SIZE),
-        index: 0
-      },
-      CHOICE_COLOR: {
-        buffer: Array(BUFFER_RING_SIZE),
-        index: 0
-      }
-    };
-    state.buffers = [
-      state['CHOICE_EMOTION_HAPPINESS'].buffer,
-      state['CHOICE_EMOTION_ENERGY'].buffer,
-      state['CHOICE_COLOR'].buffer
+    this.state = Object.values(CHOICE_TYPES).reduce(
+      (state, choiceType) =>
+        Object.assign(state, {
+          [choiceType]: {
+            buffer: Array(BUFFER_RING_SIZE),
+            index: 0
+          }
+        }),
+      {}
+    );
+    this.state.buffers = [
+      this.state[CHOICE_EMOTION_HAPPINESS].buffer,
+      this.state[CHOICE_EMOTION_ENERGY].buffer,
+      this.state[CHOICE_COLOR].buffer
     ];
-    this.state = state;
+    this.state.listening = false;
+    this.state.minTimestamp = 0;
+    concertApi.on('CONNECTED', this.handleStageChange);
+    concertApi.on('EVENT_STAGE_CHANGED', this.handleStageChange);
     concertApi.on('CHOICE_MADE', this.handleChoiceMade);
   }
 
-  async componentDidMount() {
+  async componentWillMount() {
+    Object.assign(document.body.style, styles.body);
     const idToken = (await Auth.currentSession()).getIdToken();
     concertApi.connect(idToken.getJwtToken());
   }
 
   componentWillUnmount() {
+    document.body.style.backgroundColor = null;
     concertApi.disconnect();
   }
 
-  handleChoiceMade = ({ choiceType, choice }) => {
+  handleStageChange = ({ stageId, choiceTypes, startTime }) => {
+    if (
+      stageId === 'STAGE_CHOICE_SYNESTHESIA' &&
+      choiceTypes &&
+      choiceTypes.length > 1
+    ) {
+      this.setState({
+        listening: true,
+        minTimestamp: Date.parse(startTime)
+      });
+    }
+  };
+
+  handleChoiceMade = ({ choiceType, choice, timestamp }) => {
+    if (
+      !this.state.listening ||
+      Date.parse(timestamp) < this.state.minTimestamp ||
+      !this.state[choiceType]
+    ) {
+      return;
+    }
     this.setState(({ [choiceType]: { buffer, index } }) => {
       buffer[index] = choice;
       return {
@@ -85,6 +111,10 @@ class App extends Component {
 }
 
 const styles = {
+  body: {
+    backgroundColor: 'rgb(95, 95, 95)',
+    overflow: 'hidden'
+  },
   App: {
     width: '100%',
     height: '100%',
@@ -99,4 +129,6 @@ const styles = {
   }
 };
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, {
+  includeGreetings: process.env.REACT_APP_AMPLIFY_INCLUDE_GREETING === 'true'
+});
