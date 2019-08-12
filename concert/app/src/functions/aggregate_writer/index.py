ed51780a6b43f0f47c94cabae20bc3a72841f1c4
borchardt <jackson.borchardt@gmail.com>
2019-08-12 13:42:40 -0700

    record sum and choice count instead of average

--- concert/app/src/functions/aggregate_writer/index.py
+++ concert/app/src/functions/aggregate_writer/index.py
@@ -5,6 +5,7 @@ from base64 import b64decode
 from botocore.exceptions import ClientError
 from decimal import *
 from enchanted_brain.attributes import (
+    ATTR_TOTAL_CHOICES,
     ATTR_CHOICE_VALUE_COLOR,
     ATTR_CHOICE_VALUE_EMOTION,
     ATTR_CHOICE_VALUE_CHILLS,
@@ -69,41 +70,45 @@ def add_record_to_aggregate(record):
     data = json.loads(b64decode(record["data"]).decode("utf-8"))
     timestamp = data["CHOICE_TIME"]
     choice_type = data["CHOICE_TYPE"]
-    choice_sum = data["CHOICE_SUM"]
+    choice_sum = Decimal(data["CHOICE_SUM"])
     choice_count = data["CHOICE_COUNT"]
-    choice_average = Decimal(choice_sum) / Decimal(choice_count)
 
     update_args = {
         "Key": {ATTR_RECORD_ID: RECORD_ID_AGGREGATE},
-        "ExpressionAttributeNames": {"#timestamp": timestamp},
-        "ExpressionAttributeValues": {":choice_average": choice_average},
+        "UpdateExpression": "ADD #choice_key.#timestamp.#choice_string :choice_sum, #choice_key.#timestamp.#total_choices :choice_count",
+        "ExpressionAttributeNames": {
+            "#timestamp": timestamp,
+            "#total_choices": ATTR_TOTAL_CHOICES,
+        },
+        "ExpressionAttributeValues": {
+            ":choice_sum": choice_sum,
+            ":choice_count": choice_count,
+        },
         "ReturnValues": "NONE",
     }
 
     if choice_type.startswith(CHOICE_COLOR):
         color = choice_type.split("_")[2]
         choice_type = CHOICE_COLOR
-        update_args[
-            "UpdateExpression"
-        ] = "ADD #choice_key.#timestamp.#color :choice_average"
-        update_args["ExpressionAttributeNames"]["#color"] = color
+        update_args["ExpressionAttributeNames"]["#choice_string"] = color
 
     elif choice_type.startswith("CHOICE_EMOTION"):
         emotion = choice_type[7:]
-        update_args[
-            "UpdateExpression"
-        ] = "ADD #choice_key.#timestamp.#emotion :choice_average"
-        update_args["ExpressionAttributeNames"]["#emotion"] = emotion
+        update_args["ExpressionAttributeNames"]["#choice_string"] = emotion
+        update_args["ExpressionAttributeNames"]["#total_choices"] += "_{}".format(
+            emotion
+        )
 
     else:
-        update_args["UpdateExpression"] = "ADD #choice_key.#timestamp :choice_average"
+        update_args["ExpressionAttributeNames"][
+            "#choice_string"
+        ] = ATTR_CHOICE_VALUE_CHILLS
 
     choice_key = CHOICE_TYPE_KEYS[choice_type]
     update_args["ExpressionAttributeNames"]["#choice_key"] = choice_key
 
-    if choice_key == ATTR_CHOICE_VALUE_COLOR or choice_key == ATTR_CHOICE_VALUE_EMOTION:
-        ensure_map_available_for_record = create_map_for_record_if_none_exists(
-            timestamp, choice_key
-        )
+    ensure_map_available_for_record = create_map_for_record_if_none_exists(
+        timestamp, choice_key
+    )
 
     return table.update_item(**update_args)
