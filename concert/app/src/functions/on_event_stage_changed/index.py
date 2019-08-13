b2c1c668898dcacdc5a9f6fa66d808dba67b6984
borchardt <jackson.borchardt@gmail.com>
2019-08-13 14:09:59 -0700

    update song list on event stage change

--- concert/app/src/functions/on_event_stage_changed/index.py
+++ concert/app/src/functions/on_event_stage_changed/index.py
@@ -1,16 +1,32 @@
 import boto3
 import json
 import os
+from enchanted_brain.attributes import (
+    ATTR_DISPLAY_NAME,
+    ATTR_END_TIME,
+    ATTR_RECORD_ID,
+    ATTR_SONGS,
+    ATTR_START_TIME,
+    RECORD_ID_SONG_LIST,
+)
 
 CALLBACK_GLOBAL_SNS_TOPIC_ARN = os.environ["CALLBACK_GLOBAL_SNS_TOPIC_ARN"]
+DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
+DYNAMODB_ENDPOINT = os.environ.get("DYNAMODB_ENDPOINT")
+
+dynamodb_args = {}
+if DYNAMODB_ENDPOINT:
+    dynamodb_args["endpoint_url"] = DYNAMODB_ENDPOINT
 
 sns = boto3.client("sns")
+dynamodb = boto3.resource("dynamodb", **dynamodb_args)
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
 
 def handler(event, context):
     message = json.loads(event["body"])
 
-    response = sns.publish(
+    sns_response = sns.publish(
         TopicArn=CALLBACK_GLOBAL_SNS_TOPIC_ARN,
         Message=json.dumps(message),
         MessageStructure="string",
@@ -19,4 +35,34 @@ def handler(event, context):
         },
     )
 
+    song_list_update_response = update_song_list(message)
+
     return {"statusCode": 204}
+
+
+def update_song_list(message):
+    response = None
+
+    data = message["data"]
+    display_name = data.get(ATTR_DISPLAY_NAME)
+    start_time = data.get(ATTR_START_TIME)
+    end_time = data.get(ATTR_END_TIME)
+    if display_name and start_time and end_time:
+
+        song = {
+            ATTR_DISPLAY_NAME: display_name,
+            ATTR_START_TIME: start_time,
+            ATTR_END_TIME: end_time,
+        }
+
+        update_args = {
+            "Key": {ATTR_RECORD_ID: RECORD_ID_SONG_LIST},
+            "UpdateExpression": "ADD #songs :song",
+            "ExpressionAttributeNames": {"#songs": ATTR_SONGS},
+            "ExpressionAttributeValues": {":song": song},
+            "ReturnValues": "NONE",
+        }
+
+        response = table.update_item(**update_args)
+
+    return response
---
+++ concert/app/test/on_event_stage_changed/test_on_event_stage_changed.py
@@ -0,0 +1,113 @@
+import json
+import os
+import pytest
+from botocore.exceptions import ClientError
+from botocore.stub import Stubber
+from src.functions.on_event_stage_changed.index import handler, sns, dynamodb
+
+CALLBACK_SNS_ARN = os.environ["CALLBACK_GLOBAL_SNS_TOPIC_ARN"]
+TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
+
+SUCCESS_RESPONSE = {"ResponseMetadata": {"HTTPStatusCode": 200}}
+
+
+def get_event(
+    stageId="STAGE_CHOICE_COLOR_EMOTION",
+    display_name=None,
+    start_time=None,
+    end_time=None,
+    frequency=None,
+):
+    data = {"stageId": stageId}
+    if display_name:
+        data["displayName"] = display_name
+    if start_time:
+        data["startTime"] = start_time
+    if end_time:
+        data["endTime"] = end_time
+    if frequency:
+        data["frequency"] = frequency
+    return {"body": json.dumps({"event": "EVENT_STAGE_CHANGED", "data": data})}
+
+
+@pytest.mark.parametrize(
+    "stage_id, display_name, start_time, end_time, frequency",
+    [
+        (
+            "STAGE_CHOICE_COLOR_EMOTION",
+            "Canon in D",
+            "2019-05-14T21:20:03.000Z",
+            "2019-05-14T21:21:33.000Z",
+            20,
+        ),
+        ("some other stage type", None, None, None, None),
+    ],
+)
+def test_event_stage_changed(stage_id, display_name, start_time, end_time, frequency):
+    event = get_event(stage_id, display_name, start_time, end_time, frequency)
+
+    sns_expected_params = {
+        "TopicArn": CALLBACK_SNS_ARN,
+        "Message": event["body"],
+        "MessageStructure": "string",
+        "MessageAttributes": {
+            "connection_type": {"DataType": "String", "StringValue": "GLOBAL"}
+        },
+    }
+
+    dynamo_song_list_expected_params = {
+        "TableName": TABLE_NAME,
+        "Key": {"recordId": "SONG_LIST"},
+        "UpdateExpression": "ADD #songs :song",
+        "ExpressionAttributeNames": {"#songs": "songs"},
+        "ExpressionAttributeValues": {
+            ":song": {
+                "displayName": display_name,
+                "startTime": start_time,
+                "endTime": end_time,
+            }
+        },
+        "ReturnValues": "NONE",
+    }
+
+    resp = None
+    with Stubber(sns) as sns_stub, Stubber(dynamodb.meta.client) as dynamo_stub:
+        sns_stub.add_response("publish", SUCCESS_RESPONSE, sns_expected_params)
+
+        if display_name and start_time and end_time:
+            dynamo_stub.add_response(
+                "update_item", SUCCESS_RESPONSE, dynamo_song_list_expected_params
+            )
+
+        resp = handler(event, None)
+        sns_stub.assert_no_pending_responses()
+        dynamo_stub.assert_no_pending_responses()
+
+    assert resp == {"statusCode": 204}
+
+
+def test_sns_publish_error_raises_client_error():
+    with pytest.raises(ClientError):
+        with Stubber(sns) as sns_stub:
+            sns_stub.add_client_error(
+                "publish",
+                service_message="Jeff Bezos dislikes you personally and has sabotaged your SNS endpoint",
+                service_error_code=500,
+            )
+            handler(get_event(), None)
+
+
+def test_dynamodb_update_error_raises_client_error():
+    with pytest.raises(ClientError):
+        with Stubber(sns) as sns_stub, Stubber(dynamodb.meta.client) as dynamo_stub:
+            sns_stub.add_response("publish", SUCCESS_RESPONSE)
+            dynamo_stub.add_client_error("update_item")
+            handler(
+                get_event(
+                    stageId="stage id",
+                    display_name="song name",
+                    start_time="start time",
+                    end_time="end time",
+                ),
+                None,
+            )
