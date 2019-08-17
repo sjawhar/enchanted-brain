f6a1f3c9dc839492fd7cc1eff5f144e54deee6c9
borchardt <jackson.borchardt@gmail.com>
2019-08-16 22:33:07 -0700

    use transaction for DynamoDB update

--- concert/app/src/functions/on_event_stage_changed/index.py
+++ concert/app/src/functions/on_event_stage_changed/index.py
@@ -15,78 +15,84 @@ from enchanted_brain.attributes import (
 
 CALLBACK_GLOBAL_SNS_TOPIC_ARN = os.environ["CALLBACK_GLOBAL_SNS_TOPIC_ARN"]
 DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
-DYNAMODB_ENDPOINT = os.environ.get("DYNAMODB_ENDPOINT")
-
-dynamodb_args = {}
-if DYNAMODB_ENDPOINT:
-    dynamodb_args["endpoint_url"] = DYNAMODB_ENDPOINT
 
 sns = boto3.client("sns")
-dynamodb = boto3.resource("dynamodb", **dynamodb_args)
-table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+dynamodb = boto3.client("dynamodb")
 
 
 def handler(event, context):
     message = json.loads(event["body"])
 
-    stage_record_update_response = update_stage_record(message)
+    update_event_stage_and_song_list(message)
 
-    sns_response = sns.publish(
+    sns.publish(
         TopicArn=CALLBACK_GLOBAL_SNS_TOPIC_ARN,
         Message=json.dumps(message),
         MessageStructure="string",
     )
 
-    song_list_update_response = update_song_list(message)
-
     return {"statusCode": 204}
 
 
-def update_song_list(message):
-    response = None
-
-    data = message["data"]
-    display_name = data.get(ATTR_SONG_LIST_DISPLAY_NAME)
-    start_time = data.get(ATTR_SONG_LIST_START_TIME)
-    end_time = data.get(ATTR_SONG_LIST_END_TIME)
-    if display_name and start_time and end_time:
-
-        song = [
-            {
-                ATTR_SONG_LIST_DISPLAY_NAME: display_name,
-                ATTR_SONG_LIST_START_TIME: start_time,
-                ATTR_SONG_LIST_END_TIME: end_time,
-            }
-        ]
-
-        update_args = {
-            "Key": {ATTR_RECORD_ID: RECORD_ID_SONG_LIST},
-            "UpdateExpression": "SET #songs = list_append(#songs, :song)",
-            "ExpressionAttributeNames": {"#songs": ATTR_SONG_LIST_SONGS},
-            "ExpressionAttributeValues": {":song": song},
-            "ReturnValues": "NONE",
-        }
-
-        response = table.update_item(**update_args)
-
-    return response
-
-
-def update_stage_record(message):
+def update_event_stage_and_song_list(message):
     data = copy(message["data"])
-    stage_id = data.pop(ATTR_EVENT_STAGE_ID)
 
-    update_args = {
-        "Key": {ATTR_RECORD_ID: RECORD_ID_EVENT_STAGE},
-        "UpdateExpression": "SET #stage_id = :stage_id",
-        "ExpressionAttributeNames": {"#stage_id": ATTR_EVENT_STAGE_ID},
-        "ExpressionAttributeValues": {":stage_id": stage_id},
-        "ReturnValues": "NONE",
+    event_stage_put_transaction_item = get_event_stage_put_transaction_item(data)
+    song_list_update_transaction_item = get_song_list_update_transaction_item(data)
+
+    transaction_items = [event_stage_put_transaction_item]
+    if song_list_update_transaction_item:
+        transaction_items.append(song_list_update_transaction_item)
+
+    return dynamodb.transact_write_items(TransactItems=transaction_items)
+
+
+def get_event_stage_put_transaction_item(message_data):
+    stage_id = message_data.pop(ATTR_EVENT_STAGE_ID)
+
+    event_stage_update_args = {
+        "Item": {
+            ATTR_RECORD_ID: {"S": RECORD_ID_EVENT_STAGE},
+            ATTR_EVENT_STAGE_ID: {"S": stage_id},
+        },
+        "TableName": DYNAMODB_TABLE_NAME,
     }
 
-    for key, value in data.items():
-        update_args["UpdateExpression"] += ", #{} = :{}".format(key, key)
-        update_args["ExpressionAttributeNames"]["#{}".format(key)] = key
-        update_args["ExpressionAttributeValues"][":{}".format(key)] = value
+    for key, value in message_data.items():
+        if isinstance(value, int):
+            value_data_type = "N"
+            value = str(value)
+        else:
+            value_data_type = "S"
+        event_stage_update_args["Item"][key] = {value_data_type: value}
 
-    return table.update_item(**update_args)
+    return {"Put": event_stage_update_args}
+
+
+def get_song_list_update_transaction_item(message_data):
+    display_name = message_data.get(ATTR_SONG_LIST_DISPLAY_NAME)
+    start_time = message_data.get(ATTR_SONG_LIST_START_TIME)
+    end_time = message_data.get(ATTR_SONG_LIST_END_TIME)
+
+    if not (display_name and start_time and end_time):
+        return
+
+    song = [
+        {
+            "M": {
+                ATTR_SONG_LIST_DISPLAY_NAME: {"S": display_name},
+                ATTR_SONG_LIST_START_TIME: {"S": start_time},
+                ATTR_SONG_LIST_END_TIME: {"S": end_time},
+            }
+        }
+    ]
+
+    song_list_update_args = {
+        "Key": {ATTR_RECORD_ID: {"S": RECORD_ID_SONG_LIST}},
+        "UpdateExpression": "SET #songs = list_append(#songs, :song)",
+        "ExpressionAttributeNames": {"#songs": ATTR_SONG_LIST_SONGS},
+        "ExpressionAttributeValues": {":song": {"L": song}},
+        "TableName": DYNAMODB_TABLE_NAME,
+    }
+
+    return {"Update": song_list_update_args}
--- concert/app/test/on_event_stage_changed/test_on_event_stage_changed.py
+++ concert/app/test/on_event_stage_changed/test_on_event_stage_changed.py
@@ -12,13 +12,14 @@ SUCCESS_RESPONSE = {"ResponseMetadata": {"HTTPStatusCode": 200}}
 
 
 def get_event(
-    stageId="STAGE_CHOICE_COLOR_EMOTION",
+    stage_id="STAGE_CHOICE_COLOR_EMOTION",
     display_name=None,
     start_time=None,
     end_time=None,
     frequency=None,
+    form_url=None,
 ):
-    data = {"stageId": stageId}
+    data = {"stageId": stage_id}
     if display_name:
         data["displayName"] = display_name
     if start_time:
@@ -27,23 +28,37 @@ def get_event(
         data["endTime"] = end_time
     if frequency:
         data["frequency"] = frequency
+    if form_url:
+        data["formUrl"] = form_url
     return {"body": json.dumps({"event": "EVENT_STAGE_CHANGED", "data": data})}
 
 
 @pytest.mark.parametrize(
-    "stage_id, display_name, start_time, end_time, frequency",
+    "stage_id, display_name, start_time, end_time, frequency, form_url",
     [
         (
-            "STAGE_CHOICE_COLOR_EMOTION",
+            "STAGE_CHOICE_SYNESTHESIA",
             "Canon in D",
             "2019-05-14T21:20:03.000Z",
             "2019-05-14T21:21:33.000Z",
             20,
-        )
+            None,
+        ),
+        (
+            "STAGE_CHOICE_CHILLS",
+            "Canon in E",
+            "2019-05-14T21:20:03.000Z",
+            "2019-05-14T21:21:33.000Z",
+            None,
+            None,
+        ),
+        ("STAGE_CHOICE_IMAGERY", "Canon in F", None, None, None, "website.com"),
     ],
 )
-def test_event_stage_changed(stage_id, display_name, start_time, end_time, frequency):
-    event = get_event(stage_id, display_name, start_time, end_time, frequency)
+def test_callback_message_published_on_event_stage_change(
+    stage_id, display_name, start_time, end_time, frequency, form_url
+):
+    event = get_event(stage_id, display_name, start_time, end_time, frequency, form_url)
 
     sns_expected_params = {
         "TopicArn": CALLBACK_SNS_ARN,
@@ -51,56 +66,10 @@ def test_event_stage_changed(stage_id, display_name, start_time, end_time, frequ
         "MessageStructure": "string",
     }
 
-    dynamo_song_list_expected_params = {
-        "TableName": TABLE_NAME,
-        "Key": {"recordId": "SONG_LIST"},
-        "UpdateExpression": "SET #songs = list_append(#songs, :song)",
-        "ExpressionAttributeNames": {"#songs": "songs"},
-        "ExpressionAttributeValues": {
-            ":song": [
-                {
-                    "displayName": display_name,
-                    "startTime": start_time,
-                    "endTime": end_time,
-                }
-            ]
-        },
-        "ReturnValues": "NONE",
-    }
-
-    dynamo_event_stage_expected_params = {
-        "TableName": TABLE_NAME,
-        "Key": {"recordId": "EVENT_STAGE"},
-        "UpdateExpression": "SET #stage_id = :stage_id, #displayName = :displayName, #startTime = :startTime, #endTime = :endTime, #frequency = :frequency",
-        "ExpressionAttributeNames": {
-            "#displayName": "displayName",
-            "#stage_id": "stageId",
-            "#startTime": "startTime",
-            "#endTime": "endTime",
-            "#frequency": "frequency",
-        },
-        "ExpressionAttributeValues": {
-            ":stage_id": stage_id,
-            ":displayName": display_name,
-            ":startTime": start_time,
-            ":endTime": end_time,
-            ":frequency": frequency,
-        },
-        "ReturnValues": "NONE",
-    }
-
     resp = None
-    with Stubber(sns) as sns_stub, Stubber(dynamodb.meta.client) as dynamo_stub:
+    with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+        dynamo_stub.add_response("transact_write_items", SUCCESS_RESPONSE)
         sns_stub.add_response("publish", SUCCESS_RESPONSE, sns_expected_params)
-
-        dynamo_stub.add_response(
-            "update_item", SUCCESS_RESPONSE, dynamo_event_stage_expected_params
-        )
-
-        if display_name and start_time and end_time:
-            dynamo_stub.add_response(
-                "update_item", SUCCESS_RESPONSE, dynamo_song_list_expected_params
-            )
 
         resp = handler(event, None)
         sns_stub.assert_no_pending_responses()
@@ -109,16 +78,163 @@ def test_event_stage_changed(stage_id, display_name, start_time, end_time, frequ
     assert resp == {"statusCode": 204}
 
 
+@pytest.mark.parametrize(
+    "stage_id, display_name, start_time, end_time, frequency, form_url, expected_transaction_items",
+    [
+        (
+            "STAGE_CHOICE_SYNESTHESIA",
+            "Canon in D",
+            "2019-05-14T21:20:03.000Z",
+            "2019-05-14T21:21:33.000Z",
+            20,
+            None,
+            [
+                {
+                    "Put": {
+                        "Item": {
+                            "recordId": {"S": "EVENT_STAGE"},
+                            "stageId": {"S": "STAGE_CHOICE_SYNESTHESIA"},
+                            "displayName": {"S": "Canon in D"},
+                            "startTime": {"S": "2019-05-14T21:20:03.000Z"},
+                            "endTime": {"S": "2019-05-14T21:21:33.000Z"},
+                            "frequency": {"N": "20"},
+                        },
+                        "TableName": TABLE_NAME,
+                    }
+                },
+                {
+                    "Update": {
+                        "Key": {"recordId": {"S": "SONG_LIST"}},
+                        "UpdateExpression": "SET #songs = list_append(#songs, :song)",
+                        "ExpressionAttributeNames": {"#songs": "songs"},
+                        "ExpressionAttributeValues": {
+                            ":song": {
+                                "L": [
+                                    {
+                                        "M": {
+                                            "displayName": {"S": "Canon in D"},
+                                            "startTime": {
+                                                "S": "2019-05-14T21:20:03.000Z"
+                                            },
+                                            "endTime": {
+                                                "S": "2019-05-14T21:21:33.000Z"
+                                            },
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        "TableName": TABLE_NAME,
+                    }
+                },
+            ],
+        ),
+        (
+            "STAGE_CHOICE_CHILLS",
+            "Canon in E",
+            "2019-05-14T21:20:03.000Z",
+            "2019-05-14T21:21:33.000Z",
+            None,
+            None,
+            [
+                {
+                    "Put": {
+                        "Item": {
+                            "recordId": {"S": "EVENT_STAGE"},
+                            "stageId": {"S": "STAGE_CHOICE_CHILLS"},
+                            "displayName": {"S": "Canon in E"},
+                            "startTime": {"S": "2019-05-14T21:20:03.000Z"},
+                            "endTime": {"S": "2019-05-14T21:21:33.000Z"},
+                        },
+                        "TableName": TABLE_NAME,
+                    }
+                },
+                {
+                    "Update": {
+                        "Key": {"recordId": {"S": "SONG_LIST"}},
+                        "UpdateExpression": "SET #songs = list_append(#songs, :song)",
+                        "ExpressionAttributeNames": {"#songs": "songs"},
+                        "ExpressionAttributeValues": {
+                            ":song": {
+                                "L": [
+                                    {
+                                        "M": {
+                                            "displayName": {"S": "Canon in E"},
+                                            "startTime": {
+                                                "S": "2019-05-14T21:20:03.000Z"
+                                            },
+                                            "endTime": {
+                                                "S": "2019-05-14T21:21:33.000Z"
+                                            },
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        "TableName": TABLE_NAME,
+                    }
+                },
+            ],
+        ),
+        (
+            "STAGE_CHOICE_IMAGERY",
+            None,
+            None,
+            None,
+            None,
+            "website.com",
+            [
+                {
+                    "Put": {
+                        "Item": {
+                            "recordId": {"S": "EVENT_STAGE"},
+                            "stageId": {"S": "STAGE_CHOICE_IMAGERY"},
+                            "formUrl": {"S": "website.com"},
+                        },
+                        "TableName": TABLE_NAME,
+                    }
+                }
+            ],
+        ),
+    ],
+)
+def test_event_stage_and_song_list_updated_on_event_stage_change(
+    stage_id,
+    display_name,
+    start_time,
+    end_time,
+    frequency,
+    form_url,
+    expected_transaction_items,
+):
+    event = get_event(stage_id, display_name, start_time, end_time, frequency, form_url)
+
+    dynamo_expected_params = {"TransactItems": expected_transaction_items}
+
+    resp = None
+    with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+        dynamo_stub.add_response(
+            "transact_write_items", SUCCESS_RESPONSE, dynamo_expected_params
+        )
+        sns_stub.add_response("publish", SUCCESS_RESPONSE)
+
+        resp = handler(event, None)
+        dynamo_stub.assert_no_pending_responses()
+        sns_stub.assert_no_pending_responses()
+
+    assert resp == {"statusCode": 204}
+
+
 def test_sns_publish_error_raises_client_error():
     with pytest.raises(ClientError):
-        with Stubber(sns) as sns_stub, Stubber(dynamodb.meta.client) as dynamo_stub:
-            dynamo_stub.add_response("update_item", SUCCESS_RESPONSE)
+        with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+            dynamo_stub.add_response("transact_write_items", SUCCESS_RESPONSE)
             sns_stub.add_client_error("publish")
             handler(get_event(), None)
 
 
 def test_dynamodb_update_error_raises_client_error():
     with pytest.raises(ClientError):
-        with Stubber(sns) as sns_stub, Stubber(dynamodb.meta.client) as dynamo_stub:
-            dynamo_stub.add_client_error("update_item")
+        with Stubber(dynamodb) as dynamo_stub:
+            dynamo_stub.add_client_error("transact_write_items")
             handler(get_event(), None)
