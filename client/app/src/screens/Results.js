7db83eaca62dd2a9cb3a4252cb191626af4ce850
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-08-18 20:03:39 +0200

    Added basic carousel elements to results screen

--- client/app/src/screens/Results.js
+++ client/app/src/screens/Results.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import SideSwipe from 'react-native-sideswipe';
+import Constants from 'expo-constants';
+
+import Layout from '../constants/Layout';
+
+const CARD_WIDTH = 0.9 * Layout.window.width;
+const CARD_MARGIN = 0.05 * Layout.window.width;
 
 class ResultsScreen extends Component {
+  state = {
+    currentIndex: 0,
+  };
+
+  handleIndexChange = index => this.setState({ currentIndex: index });
+
   render() {
+    const { songs } = this.props.navigation.state.params;
+    songs.sort((a, b) => a.startTime.localeCompare(b.startTime));
     return (
       <View style={styles.container}>
-        <Text>I'll be the Results screen someday.</Text>
+        <View styles={styles.header}>
+          <Text>Results</Text>
+        </View>
+        <SideSwipe
+          data={songs}
+          extractKey={({ displayName }) => displayName}
+          index={this.state.currentIndex}
+          itemWidth={Layout.window.width}
+          onIndexChange={this.handleIndexChange}
+          style={styles.carousel}
+          renderItem={({ itemIndex, currentIndex, item, animatedValue }) => {
+            return (
+              <View style={styles.card}>
+                <Text>{item.displayName}</Text>
+              </View>
+            );
+          }}
+        />
+        <Text>{this.state.currentIndex}</Text>
       </View>
     );
   }
@@ -15,9 +48,30 @@ class ResultsScreen extends Component {
 const styles = EStyleSheet.create({
   container: {
     width: '100%',
-    height: '100%',
-    alignItems: 'center',
+    height: Layout.window.height - Constants.statusBarHeight,
+    marginTop: Constants.statusBarHeight,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+  },
+  header: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  carousel: {
+    height: '90%',
+  },
+  card: {
+    width: CARD_WIDTH,
+    marginLeft: CARD_MARGIN,
+    marginRight: CARD_MARGIN,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 13,
+    flexDirection: 'column',
+    sjustifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
