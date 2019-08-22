766153734e4d3ba8fb85f8506b4fb869fa289a62
borchardt <jackson.borchardt@gmail.com>
2019-08-22 00:06:46 -0700

    songs and aggregate choice data added to message on STAGE_END

--- concert/app/src/functions/on_event_stage_changed/index.py
+++ concert/app/src/functions/on_event_stage_changed/index.py
@@ -1,15 +1,25 @@
 import boto3
 import json
 import os
+import decimal
 from enchanted_brain.attributes import (
+    ATTR_AGGREGATE_CHOICE_COUNT,
+    ATTR_AGGREGATE_CHOICE_SUM,
+    ATTR_CHOICE_TIMESTAMP,
+    ATTR_CHOICE_TYPE,
+    ATTR_CHOICE_VALUE_CHILLS,
+    ATTR_CHOICE_VALUE_COLOR,
     ATTR_EVENT_STAGE_ID,
     ATTR_RECORD_ID,
+    ATTR_SONG_LIST_CHOICES,
     ATTR_SONG_LIST_DISPLAY_NAME,
     ATTR_SONG_LIST_END_TIME,
     ATTR_SONG_LIST_SONGS,
     ATTR_SONG_LIST_START_TIME,
+    RECORD_ID_AGGREGATE,
     RECORD_ID_EVENT_STAGE,
     RECORD_ID_SONG_LIST,
+    STAGE_END,
 )
 
 CALLBACK_GLOBAL_SNS_TOPIC_ARN = os.environ["CALLBACK_GLOBAL_SNS_TOPIC_ARN"]
@@ -22,11 +32,16 @@ dynamodb = boto3.client("dynamodb")
 def handler(event, context):
     message = json.loads(event["body"])
 
+    if message["data"][ATTR_EVENT_STAGE_ID] == STAGE_END:
+        song_list, aggregate_data = get_song_list_and_aggregate_data()
+        songs_with_choices = get_songs_with_aggregate_choices(song_list, aggregate_data)
+        message["data"][ATTR_SONG_LIST_SONGS] = songs_with_choices
+
     update_event_stage_and_song_list(message)
 
     sns.publish(
         TopicArn=CALLBACK_GLOBAL_SNS_TOPIC_ARN,
-        Message=json.dumps(message),
+        Message=json.dumps(message, default=decimal_default),
         MessageStructure="string",
     )
 
@@ -88,3 +103,105 @@ def get_song_list_update_transaction_item(message_data):
     }
 
     return {"Update": song_list_update_args}
+
+
+def get_song_list_get_transaction_item():
+    serializer = boto3.dynamodb.types.TypeSerializer()
+    song_list_get_args = {
+        "Key": {ATTR_RECORD_ID: serializer.serialize(RECORD_ID_SONG_LIST)},
+        "TableName": DYNAMODB_TABLE_NAME,
+    }
+
+    return {"Get": song_list_get_args}
+
+
+def get_aggregate_data_get_transaction_item():
+    serializer = boto3.dynamodb.types.TypeSerializer()
+    aggregate_data_get_args = {
+        "Key": {ATTR_RECORD_ID: serializer.serialize(RECORD_ID_AGGREGATE)},
+        "TableName": DYNAMODB_TABLE_NAME,
+    }
+
+    return {"Get": aggregate_data_get_args}
+
+
+def get_song_list_and_aggregate_data():
+    song_list_get_transaction_item = get_song_list_get_transaction_item()
+    aggregate_data_get_transaction_item = get_aggregate_data_get_transaction_item()
+
+    transaction_items = [
+        song_list_get_transaction_item,
+        aggregate_data_get_transaction_item,
+    ]
+
+    responses = dynamodb.transact_get_items(TransactItems=transaction_items)[
+        "Responses"
+    ]
+
+    deserializer = boto3.dynamodb.types.TypeDeserializer()
+    songs = {k: deserializer.deserialize(v) for k, v in responses[0]["Item"].items()}
+    aggregate_data = {
+        k: deserializer.deserialize(v) for k, v in responses[1]["Item"].items()
+    }
+
+    return songs, aggregate_data
+
+
+def get_songs_with_aggregate_choices(song_list, aggregate_data):
+    songs = []
+    for song_metadata in song_list[ATTR_SONG_LIST_SONGS]:
+        display_name = song_metadata[ATTR_SONG_LIST_DISPLAY_NAME]
+        start_time = song_metadata[ATTR_SONG_LIST_START_TIME]
+        end_time = song_metadata[ATTR_SONG_LIST_END_TIME]
+
+        choice_type = None
+
+        for timestamp in aggregate_data[ATTR_CHOICE_VALUE_COLOR].keys():
+            if start_time <= timestamp < end_time:
+                choice_type = ATTR_CHOICE_VALUE_COLOR
+                break
+        if not choice_type:
+            choice_type = ATTR_CHOICE_VALUE_CHILLS
+
+        choices = []
+        for timestamp, aggregate_choice_data in aggregate_data[choice_type].items():
+            if start_time <= timestamp < end_time:
+                choices_at_timestamp = {ATTR_CHOICE_TIMESTAMP: timestamp}
+                if choice_type == ATTR_CHOICE_VALUE_COLOR:
+                    choices_at_timestamp.update(
+                        {
+                            k[4:]: v
+                            for k, v in aggregate_choice_data.items()
+                            if str.startswith(k, ATTR_AGGREGATE_CHOICE_SUM)
+                        }
+                    )
+                else:
+                    choices_at_timestamp.update(
+                        {
+                            ATTR_AGGREGATE_CHOICE_SUM: aggregate_choice_data[
+                                ATTR_AGGREGATE_CHOICE_SUM
+                            ],
+                            ATTR_AGGREGATE_CHOICE_COUNT: aggregate_choice_data[
+                                ATTR_AGGREGATE_CHOICE_COUNT
+                            ],
+                        }
+                    )
+                choices.append(choices_at_timestamp)
+
+        songs.append(
+            {
+                ATTR_SONG_LIST_DISPLAY_NAME: display_name,
+                ATTR_SONG_LIST_START_TIME: start_time,
+                ATTR_SONG_LIST_END_TIME: end_time,
+                ATTR_CHOICE_TYPE: choice_type,
+                ATTR_SONG_LIST_CHOICES: choices,
+            }
+        )
+
+    return songs
+
+
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    raise TypeError
--- concert/app/test/on_event_stage_changed/test_on_event_stage_changed.py
+++ concert/app/test/on_event_stage_changed/test_on_event_stage_changed.py
@@ -10,6 +10,67 @@ TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
 
 SUCCESS_RESPONSE = {"ResponseMetadata": {"HTTPStatusCode": 200}}
 
+DYNAMO_GET_SONG_LIST_AND_AGGREGATE_CHOICES_RESPONSE = {
+    "Responses": [
+        {
+            "Item": {
+                "recordId": {"S": "SONG_LIST"},
+                "songs": {
+                    "L": [
+                        {
+                            "M": {
+                                "displayName": {"S": "song title 1"},
+                                "endTime": {"S": "2019-05-14T21:16:33.000Z"},
+                                "startTime": {"S": "2019-05-14T21:15:03.000Z"},
+                            }
+                        },
+                        {
+                            "M": {
+                                "displayName": {"S": "song title 2"},
+                                "endTime": {"S": "2019-05-14T21:21:33.000Z"},
+                                "startTime": {"S": "2019-05-14T21:20:03.000Z"},
+                            }
+                        },
+                    ]
+                },
+            }
+        },
+        {
+            "Item": {
+                "recordId": {"S": "AGGREGATE"},
+                "chills": {
+                    "M": {
+                        "2019-05-14T21:20:03.000Z": {
+                            "M": {"count": {"N": "2"}, "sum": {"N": "2.5"}}
+                        },
+                        "2019-05-14T21:20:03.200Z": {
+                            "M": {"count": {"N": "1"}, "sum": {"N": "1.5"}}
+                        },
+                    }
+                },
+                "colors": {
+                    "M": {
+                        "2019-05-14T21:15:03.000Z": {
+                            "M": {
+                                "count": {"N": "20"},
+                                "sum_#ABABAB": {"N": "5"},
+                                "sum_#171717": {"N": "15"},
+                            }
+                        },
+                        "2019-05-14T21:15:23.000Z": {
+                            "M": {
+                                "count": {"N": "17"},
+                                "sum_#ABABAB": {"N": "10"},
+                                "sum_#CDCDCD": {"N": "7"},
+                            }
+                        },
+                    }
+                },
+            }
+        },
+    ]
+}
+
 
 def get_event(
     stage_id="STAGE_CHOICE_COLOR_EMOTION",
@@ -254,6 +315,193 @@ def test_event_stage_and_song_list_updated_on_event_stage_change(
 
     resp = None
     with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+        dynamo_stub.add_response(
+            "transact_write_items", SUCCESS_RESPONSE, dynamo_expected_params
+        )
+        sns_stub.add_response("publish", SUCCESS_RESPONSE)
+
+        resp = handler(event, None)
+        dynamo_stub.assert_no_pending_responses()
+        sns_stub.assert_no_pending_responses()
+
+    assert resp == {"statusCode": 204}
+
+
+def test_retrieve_song_list_and_aggregate_choices_on_stage_end():
+    event = get_event("STAGE_END")
+
+    expected_transaction_items = [
+        {"Get": {"Key": {"recordId": {"S": "SONG_LIST"}}, "TableName": TABLE_NAME}},
+        {"Get": {"Key": {"recordId": {"S": "AGGREGATE"}}, "TableName": TABLE_NAME}},
+    ]
+
+    dynamo_expected_get_params = {"TransactItems": expected_transaction_items}
+
+    resp = None
+    with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+        dynamo_stub.add_response(
+            "transact_get_items",
+            DYNAMO_GET_SONG_LIST_AND_AGGREGATE_CHOICES_RESPONSE,
+            dynamo_expected_get_params,
+        )
+        dynamo_stub.add_response("transact_write_items", SUCCESS_RESPONSE)
+        sns_stub.add_response("publish", SUCCESS_RESPONSE)
+
+        resp = handler(event, None)
+        dynamo_stub.assert_no_pending_responses()
+        sns_stub.assert_no_pending_responses()
+
+    assert resp == {"statusCode": 204}
+
+
+def test_song_list_and_aggregate_choices_sent_to_callback_on_stage_end():
+    event = get_event("STAGE_END")
+
+    expected_songs_list = [
+        {
+            "displayName": "song title 1",
+            "startTime": "2019-05-14T21:15:03.000Z",
+            "endTime": "2019-05-14T21:16:33.000Z",
+            "choiceType": "colors",
+            "choices": [
+                {
+                    "timestamp": "2019-05-14T21:15:03.000Z",
+                    "#ABABAB": 5.0,
+                    "#171717": 15.0,
+                },
+                {
+                    "timestamp": "2019-05-14T21:15:23.000Z",
+                    "#ABABAB": 10.0,
+                    "#CDCDCD": 7.0,
+                },
+            ],
+        },
+        {
+            "displayName": "song title 2",
+            "startTime": "2019-05-14T21:20:03.000Z",
+            "endTime": "2019-05-14T21:21:33.000Z",
+            "choiceType": "chills",
+            "choices": [
+                {"timestamp": "2019-05-14T21:20:03.000Z", "sum": 2.5, "count": 2.0},
+                {"timestamp": "2019-05-14T21:20:03.200Z", "sum": 1.5, "count": 1.0},
+            ],
+        },
+    ]
+
+    sns_expected_message = json.dumps(
+        {
+            "event": "EVENT_STAGE_CHANGED",
+            "data": {"stageId": "STAGE_END", "songs": expected_songs_list},
+        }
+    )
+
+    sns_expected_params = {
+        "TopicArn": CALLBACK_SNS_ARN,
+        "Message": sns_expected_message,
+        "MessageStructure": "string",
+    }
+
+    resp = None
+    with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+        dynamo_stub.add_response(
+            "transact_get_items", DYNAMO_GET_SONG_LIST_AND_AGGREGATE_CHOICES_RESPONSE
+        )
+        dynamo_stub.add_response("transact_write_items", SUCCESS_RESPONSE)
+        sns_stub.add_response("publish", SUCCESS_RESPONSE, sns_expected_params)
+
+        resp = handler(event, None)
+        dynamo_stub.assert_no_pending_responses()
+        sns_stub.assert_no_pending_responses()
+
+    assert resp == {"statusCode": 204}
+
+
+def test_song_list_and_aggregate_choices_added_to_event_stage_on_stage_end():
+    event = get_event("STAGE_END")
+
+    dynamo_put_expected_transaction_items = [
+        {
+            "Put": {
+                "Item": {
+                    "recordId": {"S": "EVENT_STAGE"},
+                    "stageId": {"S": "STAGE_END"},
+                    "songs": {
+                        "L": [
+                            {
+                                "M": {
+                                    "displayName": {"S": "song title 1"},
+                                    "startTime": {"S": "2019-05-14T21:15:03.000Z"},
+                                    "endTime": {"S": "2019-05-14T21:16:33.000Z"},
+                                    "choiceType": {"S": "colors"},
+                                    "choices": {
+                                        "L": [
+                                            {
+                                                "M": {
+                                                    "timestamp": {
+                                                        "S": "2019-05-14T21:15:03.000Z"
+                                                    },
+                                                    "#ABABAB": {"N": "5"},
+                                                    "#171717": {"N": "15"},
+                                                }
+                                            },
+                                            {
+                                                "M": {
+                                                    "timestamp": {
+                                                        "S": "2019-05-14T21:15:23.000Z"
+                                                    },
+                                                    "#ABABAB": {"N": "10"},
+                                                    "#CDCDCD": {"N": "7"},
+                                                }
+                                            },
+                                        ]
+                                    },
+                                }
+                            },
+                            {
+                                "M": {
+                                    "displayName": {"S": "song title 2"},
+                                    "startTime": {"S": "2019-05-14T21:20:03.000Z"},
+                                    "endTime": {"S": "2019-05-14T21:21:33.000Z"},
+                                    "choiceType": {"S": "chills"},
+                                    "choices": {
+                                        "L": [
+                                            {
+                                                "M": {
+                                                    "timestamp": {
+                                                        "S": "2019-05-14T21:20:03.000Z"
+                                                    },
+                                                    "sum": {"N": "2.5"},
+                                                    "count": {"N": "2"},
+                                                }
+                                            },
+                                            {
+                                                "M": {
+                                                    "timestamp": {
+                                                        "S": "2019-05-14T21:20:03.200Z"
+                                                    },
+                                                    "sum": {"N": "1.5"},
+                                                    "count": {"N": "1"},
+                                                }
+                                            },
+                                        ]
+                                    },
+                                }
+                            },
+                        ]
+                    },
+                },
+                "TableName": TABLE_NAME,
+            }
+        }
+    ]
+
+    dynamo_expected_params = {"TransactItems": dynamo_put_expected_transaction_items}
+
+    resp = None
+    with Stubber(dynamodb) as dynamo_stub, Stubber(sns) as sns_stub:
+        dynamo_stub.add_response(
+            "transact_get_items", DYNAMO_GET_SONG_LIST_AND_AGGREGATE_CHOICES_RESPONSE
+        )
         dynamo_stub.add_response(
             "transact_write_items", SUCCESS_RESPONSE, dynamo_expected_params
         )
