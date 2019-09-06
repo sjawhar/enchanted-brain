77bd57e14874bd7925cb43b4121842d59256c02f
borchardt <jackson.borchardt@gmail.com>
2019-09-06 16:48:16 -0700

    add test cases for response to s3

--- concert/app/test/prepare_stack/test_prepare_stack.py
+++ concert/app/test/prepare_stack/test_prepare_stack.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pytest
+import mock
 from botocore.exceptions import ClientError
 from botocore.stub import Stubber
 from src.functions.prepare_stack.index import handler, dynamodb, kinesis_analytics
@@ -16,7 +17,7 @@ success_response = {"ResponseMetadata": {"HTTPStatusCode": 200}}
 event = {
     "ResponseURL": "http://pre-signed-S3-url-for-response",
     "StackId": "arn:aws:cloudformation:us-west-2:123456789012:stack/stack-name/guid",
-    "RequestId": "unique id for this create request",
+    "RequestId": "unique id for this request",
     "LogicalResourceId": "MyTestResource",
 }
 context = type("obj", (object,), {"log_stream_name": "log stream name"})
@@ -75,3 +76,56 @@ def test_kinesis_analytics_application_started():
         kinesis_analytics_stub.assert_no_pending_responses()
 
     assert resp == {"statusCode": 204}
+
+
+@mock.patch('botocore.vendored.requests.put', return_value=success_response)
+def test_response_sent_to_s3(put_function):
+    expected_params = {
+        "data": json.dumps({
+            "Status": "SUCCESS",
+            "Reason": "See the details in CloudWatch Log Stream: log stream name",
+            "StackId": "arn:aws:cloudformation:us-west-2:123456789012:stack/stack-name/guid",
+            "RequestId": "unique id for this request",
+            "LogicalResourceId": "MyTestResource",
+            "PhysicalResourceId": "MyTestResource"}),
+        "headers": {"content-length": "296", "content-type": ""}
+    }
+    resp = None
+    with Stubber(dynamodb.meta.client) as dynamodb_stub, Stubber(
+        kinesis_analytics
+    ) as kinesis_analytics_stub:
+        dynamodb_stub.add_response("update_item", success_response)
+        kinesis_analytics_stub.add_response(
+            "start_application", success_response
+        )
+        resp = handler(event, context)
+        assert put_function.called
+        request_args, requests_kwargs = put_function.call_args
+        assert requests_kwargs == expected_params
+
+    assert resp == {"statusCode": 204}
+
+
+@mock.patch('botocore.vendored.requests.put', return_value=success_response)
+def test_failure_response_sent_to_s3_on_error(put_function):
+    expected_params = {
+        "data": json.dumps({
+            "Status": "FAILED",
+            "Reason": "See the details in CloudWatch Log Stream: log stream name",
+            "StackId": "arn:aws:cloudformation:us-west-2:123456789012:stack/stack-name/guid",
+            "RequestId": "unique id for this request",
+            "LogicalResourceId": "MyTestResource",
+            "PhysicalResourceId": "MyTestResource"}),
+        "headers": {"content-length": "295", "content-type": ""}
+    }
+
+    with pytest.raises(ClientError):
+        with Stubber(dynamodb.meta.client) as dynamodb_stub, Stubber(
+            kinesis_analytics
+        ) as kinesis_analytics_stub:
+            dynamodb_stub.add_client_error("update_item")
+            resp = handler(event, context)
+
+    assert put_function.called
+    request_args, requests_kwargs = put_function.call_args
+    assert requests_kwargs == expected_params
