c661878c1b1ea31606ffd0240609cfcffec85cc5
Sami Jawhar <sami.n.jawhar@gmail.com>
2019-09-12 14:24:45 +0200

    Simiplified stubs

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -89,8 +89,8 @@ I18n.setLanguage(store.getState().language);
 
 class App extends React.Component {
   async componentDidMount() {
-    const idToken = (await Auth.currentSession()).getIdToken();
-    store.dispatch(actions.setUID(idToken.payload['cognito:username']));
+    const { 'cognito:username': username } = (await Auth.currentSession()).getIdToken().payload;
+    store.dispatch(actions.setUID(username));
     concertApi.on(CONNECTED, this.handleStageNavigation);
     concertApi.on(EVENT_STAGE_CHANGED, this.handleStageNavigation);
   }
